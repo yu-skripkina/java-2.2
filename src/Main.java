756ddeaad9bf242848a84b68e6d25622945269ec
Yuliya Skripkina <yu.skripkina@gmail.com>
2021-09-23 11:29:08 +0300

    Changed Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         int initialBalance = 200 ;
-        int amount = 2222 ;
+        int amount = 2000 ;
         int bonusPrise = 100 ;
-        int bonus = amount / bonusPrise ;
+        int bonus;
+        if (amount <= 1000) { bonus = 0 ;
+        }
+        else { bonus = amount / bonusPrise;
+        }
         int finalBalance = initialBalance + bonus ;
-                if (amount <= 1000) {
-            bonus = 0 ;
-                    }
+
+
         System.out.println("Бонусы" + bonus);
         System.out.println("Итоговый баланс" + finalBalance);
     }
